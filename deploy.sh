454c0de9eab6f67ccab371245c757b4c33ff9e2f
raihan0824 <mraihanafiandi@gmail.com>
2025-04-24 17:26:41 +0700

    refactor the helm chart's values

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
-kubectl delete configmap dynamo-config
-kubectl create configmap dynamo-config --from-file=/Users/raihanafiandi/Documents/Lintasarta/dynamo/deploy/Kubernetes/pipeline/configs -n dynamo-cloud
+kubectl delete configmap dynamo-config -n dynamo-cloud
+kubectl create configmap dynamo-config --from-file=configs -n dynamo-cloud
 
 helm upgrade -i dynamo-llm ./chart \
-    -f pipeline-values.yaml \
-    --set image=dekaregistry.cloudeka.id/cloudeka-system/dynamo-pipeline:0.0.2-nonroot \
-    --set dynamoIdentifier="graphs.agg_router:Frontend" \
+  --set image=dekaregistry.cloudeka.id/cloudeka-system/dynamo-pipeline:0.0.2-nonroot
